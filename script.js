0aa8aadc3374975d8a490ed7d0422f51de677bb0
rabinpatra16-dot <rabinpatra16@gmail.com>
2026-05-17 02:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ function typeWriter() {
 window.onload = function() {
   document.querySelector(".hero p").innerHTML = "";
   typeWriter();
-}
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  document.querySelectorAll(".card, .contact").forEach(el => {
+    observer.observe(el);
+  });
+};
+
 function sendMessage() {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
@@ -22,10 +35,19 @@ function sendMessage() {
   if (name === "" || email === "" || message === "") {
     alert("সব field পূরণ করো! 😅");
   } else {
-    document.getElementById("successMsg").innerHTML = 
-      "✅ ধন্যবাদ " + name + "! আমি শীঘ্রই তোমার সাথে যোগাযোগ করব!";
+    document.getElementById("successMsg").innerHTML =
+      "✅ ধন্যবাদ " + name + "! আমি শীঘ্রই যোগাযোগ করব!";
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("message").value = "";
   }
+}
+function openImg(el) {
+  const img = el.querySelector("img").src;
+  document.getElementById("lightbox-img").src = img;
+  document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeImg() {
+  document.getElementById("lightbox").style.display = "none";
 }
